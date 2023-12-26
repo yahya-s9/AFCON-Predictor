cb04294c5b21af04e7f187f05dc1a14e42579622
yahya-s9 <yahya.qt@berkeley.edu>
2023-12-26 08:28:15 -0800

    sorting functionality done

--- AFCON-Predictor/src/App.tsx
+++ AFCON-Predictor/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import GroupTable from './components/GroupTable'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const groupA = ['Ivory Coast','Nigeria','Equatorial Guinea','Guinea-Bissau'];
+  const groupB = ['Egypt','Ghana','Cape Verde','Mozambique'];
+  const groupC = ['Senegal','Cameroon','Guinea','Gambia'];
+  const groupD = ['Algeria','Burkina Faso','Mauritania','Angola'];
+  const groupE = ['Tunisia','Mali','South Africa','Namibia'];
+  const groupF = ['Morocco','DR Congo','Zambia','Tanzania'];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GroupTable countriesList={groupA} groupLetter={"A"}/>
+      <GroupTable countriesList={groupB} groupLetter={"B"}/>
+      <GroupTable countriesList={groupC} groupLetter={"C"}/>
+      <GroupTable countriesList={groupD} groupLetter={"D"}/>
+      <GroupTable countriesList={groupE} groupLetter={"E"}/>
+      <GroupTable countriesList={groupF} groupLetter={"F"}/>
+
     </>
   )
 }
 
 export default App
+
